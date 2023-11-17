59e90f08f6d45d59057b2dc7efca0ea613b17ec3
Sohyeon Oh <ohsohyeon24@gmail.com>
2023-11-17 10:40:41 +0100

    Time: 61 ms (39.01%) | Memory: 44.7 MB (43.28%) - LeetSync

--- 2761-array-reduce-transformation/array-reduce-transformation.ts
+++ 2761-array-reduce-transformation/array-reduce-transformation.ts
@@ -4,10 +4,6 @@ function reduce(nums: number[], fn: Fn, init: number): number {
 
     let res = init;
 
-    if (nums.length <= 0) {
-        return res;
-    }
-    
     for (let i=0; i<nums.length; i++) {
         res = fn(res, nums[i]);
     }
