f57652bc5411ae0f9007e247c325d963622d7565
Sohyeon Oh <ohsohyeon24@gmail.com>
2023-11-21 15:15:36 +0100

    Time: 54 ms (92.64%) | Memory: 43.3 MB (20.86%) - LeetSync

--- 2859-add-two-promises/add-two-promises.ts
+++ 2859-add-two-promises/add-two-promises.ts
@@ -1,19 +1,21 @@
 type P = Promise<number>
 
+/**
+    * both return a Single Promise.
+
+    Promise.all
+    - Return when all promises in input are fulfill.
+    - If any Promise is failed, then there are no return(Only return the first error of rejections).
+    - Element : only have value
+
+    Promise.allSettled
+    - Return when all promises in input are settled (means finished to execute).
+    - If any Promise is failed, it returns the rejected Object with status "rejected" and reason.
+    - Element : {stauts: string, value: typeof resolved value}
+*/
+
 async function addTwoPromises(promise1: P, promise2: P): P {
 	const [ans1, ans2] = await Promise.all([promise1, promise2]);
-
-    /**
-        * both return a Single Promise.
-
-        Promise.all
-        - Return when all promises in input are fulfill.
-        - If any Promise is failed, then there are no return(Only return the first error of rejections).
-
-        Promise.allSettled
-        - Return when all promises in input are settled (means finished to execute).
-        - If any Promise is failed, it returns the rejected Object with status "rejected" and reason.
-     */
 
     return ans1 + ans2;
 };
