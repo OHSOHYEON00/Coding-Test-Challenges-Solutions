46c27fb9c873d01b44854adf1850d5f79461cc83
OHSOHYEON00 <ohsohyeon24@gmail.com>
2024-01-03 22:49:17 +0100

    feat: array easy problem 36 solved

---
+++ Easy/36_Search Insert Position.ts
@@ -0,0 +1,40 @@
+/*
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
+ */
+
+function searchInsert(nums: number[], target: number): number {
+  let min = 0;
+  let max = nums.length;
+  let middle = Math.floor((max - min) / 2);
+  let result = 0;
+
+  if (target < nums[min]) {
+    return 0;
+  } else if (target > nums[max - 1]) {
+    return max;
+  }
+
+  while (true) {
+    const current = nums[middle];
+
+    if (current === target) {
+      result = middle;
+      break;
+    } else {
+      if (middle === min) {
+        result = middle + 1;
+        break;
+      }
+
+      if (current > target) {
+        max = middle;
+        middle = Math.floor((max - min) / 2);
+      } else {
+        min = middle;
+        middle = middle + Math.floor((max - min) / 2);
+      }
+    }
+  }
+  return result;
+}
