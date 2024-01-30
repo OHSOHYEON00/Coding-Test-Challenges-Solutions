92b30aa58ab17997936bfabf0c4b77fdbb768a9b
Sohyeon Oh <ohsohyeon24@gmail.com>
2024-01-30 16:08:20 +0100

    Time: 68 ms (37.76%) | Memory: 52.2 MB (18.01%) - LeetSync

--- 169-majority-element/majority-element.ts
+++ 169-majority-element/majority-element.ts
@@ -1,4 +1,27 @@
 function majorityElement(nums: number[]): number {
+
+    // don't need to care about to get exact count
+    // Goal: finding the majority number
+
+    let candidate: number = 0;
+    let count = 0;
+
+    nums.forEach((num) => {
+        if (count === 0) {
+            candidate = num;
+        }
+
+        count += (num === candidate) ? 1 : -1;
+    })
+
+    return candidate;
+
+};
+
+
+
+/**
+function majorityElement(nums: number[]): number {
     
     let result = nums.concat();
     result = result.sort((a,b) => a-b);
@@ -18,3 +41,4 @@ function majorityElement(nums: number[]): number {
 
     return output;
 };
+ */
