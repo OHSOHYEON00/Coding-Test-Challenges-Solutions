3c0672d528a731ef4800f48ba98cf91f80c069a2
Sohyeon Oh <ohsohyeon24@gmail.com>
2024-03-05 17:32:35 +0100

    Time: 59 ms (51.72%) | Memory: 51.6 MB (38.12%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.ts
+++ 88-merge-sorted-array/merge-sorted-array.ts
@@ -7,8 +7,7 @@ function merge(nums1: number[], m: number, nums2: number[], n: number): void {
         nums1.length = 0;
         nums1.push(...nums2);
     } else if (n !== 0) {
-        nums1.splice(m, (nums1.length - m + 1)); 
-        nums1.push(...nums2);
+        nums1.splice(m, (nums1.length - m + 1), ...nums2); 
         nums1.sort((a,b) => a-b);
     }
 };
